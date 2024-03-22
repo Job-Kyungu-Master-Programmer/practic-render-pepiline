900131d289c2d679a793e8a8cb63d59def2f2f0c
Developer Job <developerjob@Developers-Mac-mini.local>
2024-03-22 07:21:17 -0700

    Add file index.html

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 
 app.get('/health', (request, response) => {
-    response.send('<h1> This okay full </h1>')
+    response.send('<h1> This Health </h1>')
+})
+
+app.get('/ping', (request, response) => {
+    response.sendFile(path.join(__dirname, 'index.html'))
 })
 
 const PORT = process.env.PORT || 3002
 app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`)
 })
+
